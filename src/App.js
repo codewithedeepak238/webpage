882012b81d9c6567a1ec8bd582d24fdaad62db1b
Deepak Saraswat <133565275+codewithedeepak238@users.noreply.github.com>
2024-05-27 02:05:12 +0000

    app.js updated

--- src/App.js
+++ src/App.js
@@ -3,8 +3,34 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Welcome to My Profile</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, sit nisi
+          a, laborum id dolorem earum reprehenderit quibusdam ad ipsum
+          doloribus! Odit sit a illo iure repudiandae rerum porro doloremque!
+        </p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, sit nisi
+          a, laborum id dolorem earum reprehenderit quibusdam ad ipsum
+          doloribus! Odit sit a illo iure repudiandae rerum porro doloremque!
+          Visit:{" "}
+          <a
+            href="https://www.linkedin.com/in/deepak-saraswat-b87820277/"
+            target="_blank"
+          >
+            Visit Me
+          </a>
+        </p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
+          ducimus.
+        </p>
+      </div>
     </div>
   );
 }
